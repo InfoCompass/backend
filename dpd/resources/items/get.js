8795490da2a37854055df082a4d2dea12691b4bc
rhotep <andreas.pittrich@posteo.de>
2024-09-16 22:06:38 +0200

    blocked proposals

--- dpd/resources/items/get.js
+++ dpd/resources/items/get.js
@@ -37,18 +37,18 @@ if(internal || (me && me.privileges.indexOf('edit_items') != -1 )) {
         )
     }
 
-    this.proposals = []
+    // this.proposals = []
 
-    $addCallback()
-    dpd.items.get({proposalFor: this.id})
-    .then(
-        proposals => { 
-            this.proposals.push(...proposals)
-            $finishCallback()
-        },
+    // $addCallback()
+    // dpd.items.get({proposalFor: this.id})
+    // .then(
+    //     proposals => { 
+    //         this.proposals.push(...proposals)
+    //         $finishCallback()
+    //     },
 
-        $finishCallback()
-    )
+    //     $finishCallback()
+    // )
 
 } else {
     this.editingNote    = undefined
