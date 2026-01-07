c5515a5bf45a75cfbbc0978d04807f56decc97c2
rhotep <andreas.pittrich@posteo.de>
2026-01-07 03:25:59 +0100

    added zip search

--- public_api/nominatim.js
+++ public_api/nominatim.js
@@ -87,11 +87,16 @@ export class Nominatim {
 
 									const params = new URLSearchParams({...fullQuery, state})
 
+									console.log({params})						
+
 									return await this.nominatimRequest(params)
 									
 								})
 
 		const data 			= 	await Promise.all(searchResults)
+
+		console.log({data})
+
 		const results 		= 	data.flat()
 								.filter( result => result.address.postcode == request.postalcode) 
 								.map( ({lat, lon, display_name }) => ({lat,lon, display_name}))
